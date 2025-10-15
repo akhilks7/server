24d576c09ce3893980657a26984bedb6dbd0c0b5
akhilks7 <akhilksks47@gmail.com>
2025-10-15 12:29:02 +0530

    adding index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// import jsonserver
+const JSONServer = require('json-server')
+//create serrver for running json server
+const rBuilder = JSONServer.create()
+
+// CREATEING MIDDLEWARE 
+const middleware = JSONServer.defaults()
+//  imoprt db.json file
+const router = JSONServer.router("db.json")
+//define port to run server
+const PORT = 3000
+//use middle ware
+rBuilder.use(middleware)
+//use router
+rBuilder.use(router)
+//tell sserver to listen for client request
+rBuilder.listen(PORT,()=>{
+    console.log(`resume bulder started port at number ${PORT}`);
+    
+})
